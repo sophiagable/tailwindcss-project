b46dd52f82573592d7ef55eeec61dbd3d17a51ff
sophiagable <sophiagable>
2020-04-07 15:00:54 +0800

    postcss-purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,26 @@
-module.exports = {
+/** 此工具是找所有檔案裏有用到 tailwindcss 的 class */
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+    // Specify the paths to all of the template files in your project 
+    content: [
+      './src/**/*.html',
+      './src/**/*.vue',
+      './src/**/*.jsx',
+      // etc.
+    ],
+  
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  })
+
+  /** 如果是正式環境，則只包用到的class，如果是開發環境則全部包 */
+  
+  module.exports = {
     plugins: [
       require('tailwindcss'),
-      require('autoprefixer'), // 前綴字處理，版本問題
+      require('autoprefixer'),
+      ...process.env.NODE_ENV === 'production'
+        ? [purgecss]
+        : []
     ]
   }
